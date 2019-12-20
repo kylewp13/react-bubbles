914d2e672d00ee0b96631f773eca00c5f44a3aa5
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2019-12-20 11:56:00 -0500

    update to baseURL added /api

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosWithAuth = () => {
   return axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "http://localhost:5001/api",
     headers: {
       //Returns the current value associated with the given key, or null if the given key does not exist in the list associated with the object.
       authorization: localStorage.getItem('token') 
