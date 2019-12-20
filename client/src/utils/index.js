e3ea82b6b6b1cf0f85464b1c21c84de853197f17
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2019-12-20 11:13:01 -0500

    added axios with Auth into utils

---
+++ client/src/utils/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+  return axios.create({
+    baseURL: "http://localhost:5001",
+    headers: {
+      //Returns the current value associated with the given key, or null if the given key does not exist in the list associated with the object.
+      authorization: localStorage.getItem('token') 
+    }
+  })
+};
+
+export default axiosWithAuth;
